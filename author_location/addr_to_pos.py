9521636b1d7e606c203265c0a38c3d336c5b7bb2
yuqil <yuqil@yuqils-MBP.wv.cc.cmu.edu>
2016-12-03 14:58:45 -0500

    data + bug fix

--- author_location/addr_to_pos.py
+++ author_location/addr_to_pos.py
@@ -1,20 +1,23 @@
 import geocoder
 import requests
-
-
-g = geocoder.google('Mountain View, CA')
-g.latlng
-
 file = open("author_address.txt")
-output = open("author_latlng.txt", "wb")
+output = open("author_latlng_yuqi2.txt", "wb")
+
+print "start"
+startline = 14616 + 2500
+i = 0
 
 for line in file:
     token = line.split("\t")
     name = token[0]
     addr = token[1]
+
+    i += 1
+    if i <= startline:
+        continue
     try:
         g = geocoder.google(token[1])
-        print g.latlng
+        print i, g.latlng
         if len(g.latlng) == 2:
             output.write(name + "\t" + str(g.latlng[0]) + "\t" + str(g.latlng[1]) + "\n")
     except requests.exceptions.ReadTimeout:
